8c27488e09103008300286575a8a9d7902a2f2d4
DmytroMitin <dmitin3@gmail.com>
2023-11-11 08:42:35 +0000

    Update version of pureconfig

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -8,7 +8,7 @@ object Dependencies {
     val fs2                 = "2.5.4"
     val http4s              = "0.21.22"
     val circe               = "0.13.0"
-    val pureConfig          = "0.14.1"
+    val pureConfig          = "0.17.4"
 
     val kindProjector       = "0.10.3"
     val logback             = "1.2.3"
