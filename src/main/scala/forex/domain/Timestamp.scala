87317b218f93300c8b2914268905201b83d92e92
DmytroMitin <dmitin3@gmail.com>
2023-11-12 16:38:41 +0000

    Add caching (a local proxy)

--- src/main/scala/forex/domain/Timestamp.scala
+++ src/main/scala/forex/domain/Timestamp.scala
@@ -2,9 +2,15 @@ package forex.domain
 
 import java.time.OffsetDateTime
 
-case class Timestamp(value: OffsetDateTime) extends AnyVal
+case class Timestamp(value: OffsetDateTime) extends AnyVal {
+  def isNotOlderThan5Minutes: Boolean =
+    value.isAfter(Timestamp.nowMinus5Minutes.value)
+}
 
 object Timestamp {
   def now: Timestamp =
     Timestamp(OffsetDateTime.now)
+
+  def nowMinus5Minutes: Timestamp =
+    Timestamp(OffsetDateTime.now.minusMinutes(5))
 }
--- src/main/scala/forex/services/rates/interpreters/OneFrame.scala
+++ src/main/scala/forex/services/rates/interpreters/OneFrame.scala
@@ -1,31 +1,48 @@
 package forex.services.rates.interpreters
 
 import cats.effect.Sync
+import cats.syntax.applicative._
 import cats.syntax.either._
 import cats.syntax.functor._
 import cats.syntax.show._
-import forex.domain.{Rate, Timestamp}
+import forex.domain.Rate
 import forex.http.rates.Protocol.GetOneFrameApiResponse
 import forex.services.rates.Algebra
 import forex.services.rates.errors._
 import org.http4s.client.Client
 import org.http4s.implicits._
 import org.http4s.{Header, Request}
+import org.log4s.getLogger
 
 class OneFrame[F[_]: Sync](client: Client[F]) extends Algebra[F] {
 
-  // TODO make domain, port, token configurable
-  override def get(pair: Rate.Pair): F[Error Either Rate] = {
-    val request = Request[F](
-      uri = uri"http://localhost:8080/rates" +? ("pair", pair.from.show + pair.to.show)
-    ).putHeaders(Header("token", "10dc303535874aeccc86a8251e6992f5"))
-    println(s"request=$request")
+  private val logger = getLogger
 
-    for {
-      response <- client.expect[List[GetOneFrameApiResponse]](request)
-    } yield {
-      println(s"response=$response")
-      Rate(pair, response.head.price, Timestamp.now).asRight[Error]
+  private var cache: Map[Rate.Pair, Rate] = Map()
+
+  // TODO make domain, port, token configurable
+  override def get(pair: Rate.Pair): F[Error Either Rate] =
+    cache.get(pair) match {
+      case Some(rate) if rate.timestamp.isNotOlderThan5Minutes =>
+        rate.asRight[Error].pure[F]
+      case _ =>
+        val request = Request[F](
+          uri = uri"http://localhost:8080/rates" +? ("pair", pair.from.show + pair.to.show)
+        ).putHeaders(Header("token", "10dc303535874aeccc86a8251e6992f5"))
+        logger.info(s"One-frame request: $request")
+
+        for {
+          response <- client.expect[List[GetOneFrameApiResponse]](request)
+        } yield {
+          logger.info(s"One-frame response: $response")
+          response.headOption match {
+            case Some(head) =>
+              val newRate = Rate(pair, head.price, head.time_stamp)
+              cache += (pair -> newRate)
+              newRate.asRight[Error]
+            case None =>
+              Error.OneFrameLookupFailed("One-Frame service returned empty result").asLeft[Rate]
+          }
+        }
     }
-  }
 }
