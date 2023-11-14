9b3b8740e0c74522dc8a8d43a776027a187139e2
DmytroMitin <dmitin3@gmail.com>
2023-11-14 05:33:45 +0000

    Update test descriptions

--- src/test/scala/forex/ActualCacheSpec.scala
+++ src/test/scala/forex/ActualCacheSpec.scala
@@ -6,11 +6,11 @@ import org.http4s
 
 class ActualCacheSpec extends SpecHelpers(Timestamp.now) {
 
-  "Caching service" should "firstly return rate from One-Frame" in {
+  "Caching service with actual cache" should "firstly return rate from One-Frame" in {
     check[GetApiResponse](response, http4s.Status.Ok, Some(expected)) should be(true)
   }
 
-  "Caching service" should "return rate from cache" in {
+  "Caching service with actual cache" should "secondly return rate from cache" in {
     getOneFrameApiResponse = getOneFrameApiResponse2
 
     check[GetApiResponse](response, http4s.Status.Ok, Some(expected)) should be(true)
--- src/test/scala/forex/OutdatedCacheSpec.scala
+++ src/test/scala/forex/OutdatedCacheSpec.scala
@@ -6,11 +6,11 @@ import org.http4s
 
 class OutdatedCacheSpec extends SpecHelpers(Timestamp.nowMinus5Minutes) {
 
-  "Caching service" should "firstly return rate from One-Frame" in {
+  "Caching service with outdated cache" should "firstly return rate from One-Frame" in {
     check[GetApiResponse](response, http4s.Status.Ok, Some(expected)) should be(true)
   }
 
-  "Caching service" should "return rate from One-Frame for outdated rate in cache" in {
+  "Caching service with outdated cache" should "secondly return rate from One-Frame too" in {
     getOneFrameApiResponse = getOneFrameApiResponse2
 
     check[GetApiResponse](response, http4s.Status.Ok, Some(expected1)) should be(true)
